54e85d7458c45e8da3a5f9839a6c4c3e0f9132cc
denialofsandwich <mail@farene.de>
2018-05-12 12:33:17 +0200

    Update: README.md

--- setup.sh
+++ setup.sh
@@ -5,8 +5,12 @@ cd "$SPWD"
 
 mkdir -P /etc/versa
 cp versa-config-example.sh /etc/versa/config.sh
+chown root:root /etc/versa/config.sh
+chmod 755 /etc/versa/config.sh
 
 rm /usr/bin/versa 2> /dev/null
 cp -f versa.sh /usr/bin/versa
+chown root:root /usr/bin/versa
+chmod 755 /usr/bin/versa
 
 echo "Done!"
